aa75dfc893b1719b03d5f58c8d3384c4671a5a18
sachin <sachinbhosale29031995@gmail.com>
2025-05-04 10:22:22 +0530

    Updates

---
+++ precache-manifest.8d150f3a7c740cd6fa52f5155a70d720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449f899798f886b4e1be1c26536b92ec",
+    "url": "/news_blogs/index.html"
+  },
+  {
+    "revision": "a136b2fc642963a81f3d",
+    "url": "/news_blogs/static/js/2.164b0737.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/news_blogs/static/js/2.164b0737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ebb581e609e00a32dd",
+    "url": "/news_blogs/static/js/main.e47112fb.chunk.js"
+  },
+  {
+    "revision": "0dec5fe192599ebb5363",
+    "url": "/news_blogs/static/js/runtime-main.2bd1e942.js"
+  },
+  {
+    "revision": "fae0e57fe07560137edcf6df6d51ca86",
+    "url": "/news_blogs/static/media/post_1.fae0e57f.png"
+  },
+  {
+    "revision": "ef159b5364c720286bf3bc560d344606",
+    "url": "/news_blogs/static/media/sample.ef159b53.md"
+  }
+]);
